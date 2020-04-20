4e9341098feabc5807b60d1ff1126cd3666e1f00
hshine1226 <hshine1226@gmail.com>
2020-04-20 21:29:11 +0900

    #2.5 if, else, and, or

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const title = document.getElementById("title");
-console.log(title);
-
-title.innerHTML = "Hello from JS";
-
-function handleClick(event){
-    title.style.color = 'blue';
+if(10 === 5){
+    console.log('hi');
 }
-title.addEventListener('click', handleClick);
+else{
+    console.log('ho');
+}
+
+if(10 > 5 && "junhyuk" === "junhyuk"){
+    console.log('yes');
+}
+else{
+    console.log('no');
+}
+
+if(10 > 5 || "junk" === "junhyuk"){
+    console.log('yes');
+}
+else{
+    console.log('no');
+}
+
+const age = prompt("How old are you?"); // 이건 완전 old js
+if (age > 18) {
+    console.log("you can drink");
+}
+else{
+    console.log("you can't")
+}
+
